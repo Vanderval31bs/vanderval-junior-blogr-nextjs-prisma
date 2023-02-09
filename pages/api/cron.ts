31ee1da9d887b18904d1c65d08609b4574e57b44
Vanderval31bs <56770214+Vanderval31bs@users.noreply.github.com>
2023-02-09 11:05:36 -0300

    Update cron.ts

--- pages/api/cron.ts
+++ pages/api/cron.ts
@@ -1,4 +1,6 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";;
+const sendgrid = require("@sendgrid/mail");
+sendgrid.setApiKey(process.env.SENDGRID_API);
 
 export default async function handler(
   req: NextApiRequest,
@@ -10,6 +12,18 @@ export default async function handler(
 
       // if (authorization === `Bearer ${process.env.API_SECRET_KEY}`) {
       console.log("succeded to call!");
+      sendgrid.send({
+        to: "vander31bs@gmail.com",
+        text: "EMAIL TESTE 1",
+        from: "ambitus@w2s3.com.br",
+        subject: "TITLE 1",
+      });
+      sendgrid.send({
+        to: "gusttavlang@gmail.com",
+        text: "EMAIL TESTE 2",
+        from: "ambitus@w2s3.com.br",
+        subject: "TITLE 2",
+      });
       res.status(200).json({ success: true });
       //   } else {
       //     res.status(401).json({ success: false });
